914c861f6932d95f781eef040f05e049a5ba2f45
vladyslavvovchenko <vladyslavvovchenko1992@gmail.com>
2023-03-28 22:23:21 +0300

    body no-scroll

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,7 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open-desk");
     refs.modal.classList.toggle("is-hidden");
   }
 })();
